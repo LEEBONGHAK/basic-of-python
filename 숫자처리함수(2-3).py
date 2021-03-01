f65fe195a2ccdad026301d5cc9c6dc0352fd2632
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 12:14:17 +0900

    Create 숫자처리함수(2-3).py

---
+++ 숫자처리함수(2-3).py
@@ -0,0 +1,14 @@
+# 3-3 숫자 처리 함수
+from math import *  # math 라이브러리 불러오기
+
+print(abs(-5))  # 5 / abs() : 절대값 반환
+print(pow(4, 2))  # 4^2 = 4*4 = 16 / pow(a, b) = a^b
+print(max(5, 12))  # 12 / max(a, b) : 최대값 반환
+print(min(5, 12))  # 5 / min(a, b) : 최소값 변환
+print(round(3.14))  # 3 / round() : 반올림
+print(round(4.99))  # 5
+
+# math 라이브러리를 사용한 것들
+print(floor(4.99))  # 4 / floor() : 내림
+print(ceil(3.14))  # 4 / ceil() : 올림
+print(sqrt(16))  # 4 / squrt() : 제곱근
