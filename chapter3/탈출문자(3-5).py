f6c83eb5430138d4c32d0e1f7804d165cfedc102
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 13:39:17 +0900

    Create 탈출문자(3-5).py

---
+++ chapter3/탈출문자(3-5).py
@@ -0,0 +1,22 @@
+# 4-5 탈출문
+# 자
+# \n : 줄바꿈
+print("백문이 불여일견\n백견이 불여일타")
+
+# \" \' : 문장 내에서 따옴표
+print("저는 '나도코딩'입니다.")
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.")  # \" : 문자 내 " 출력을 도와줌
+print("저는 \'나도코딩\'입니다.")  # \' : 문자 내 ' 출력을 도와줌
+
+# \\ : 문장 내에서 \
+print("C:\\Users\\이봉학\\Desktop\\python workspace")
+
+# \r : 커서를 맨 앞으로 이동
+print("Red Apple\rPine")
+
+# \b : 백스페이스(한 글자 삭제)
+print("Redd\bApple")
+
+# \t : 탭
+print("Red\tApple")
