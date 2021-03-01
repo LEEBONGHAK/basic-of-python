fa1ac23d3856a1ec431e77c52d8824a9ae7100b7
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 15:42:52 +0900

    Create 키워드값(6-4).py

---
+++ chapter6/키워드값(6-4).py
@@ -0,0 +1,6 @@
+# 6-4 키워드 값
+def profile2(name, age, main_lang):
+    print(name, age, main_lang)
+
+profile2(name = "유재석", main_lang = "파이썬", age = 20) # 유재석 20 파이썬
+profile2(main_lang = "자바", name = "김태호", age = 25) # 김태호 25 자바
