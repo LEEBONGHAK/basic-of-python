7bcc015a3a0310c0ca9a0a9d77f0953b3651930f
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 12:43:31 +0900

    created 퀴즈2(2-5).py and 문자열(3-1).py

---
+++ chapter3/문자열(3-1).py
@@ -0,0 +1,15 @@
+# 3-1 문자열
+
+# "" 와 ''는 문자열을 적는데 차이가 없다.
+sentence = '나는 소년입니다'
+print(sentence)
+
+sentence2 = "파이썬은 쉬워요"
+print(sentence2)
+
+# """ : 문자열을 한 줄이 아닌 여러줄에 걸쳐서 작성할 수 있게 만들어 줌
+sentence3 = """
+나는 소년이고,
+파이썬은 쉬워요
+"""
+print(sentence3)
