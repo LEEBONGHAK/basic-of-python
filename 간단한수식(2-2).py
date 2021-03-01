e724aecce7c2818551ed9e605d94966be1a93bf5
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 12:10:52 +0900

    Create 간단한수식(2-2).py

---
+++ 간단한수식(2-2).py
@@ -0,0 +1,25 @@
+# 2-2 간단한 수식
+
+print(2 + 3 * 4) # 14
+print((2 + 3) * 4) # 20
+
+number = 2 + 3 * 4 # 14 (변수 사용)
+print(number)
+
+number = number + 2 # 16
+print(number)
+
+number += 2 # 9번 문장과 같은 의미
+print(number) # 18
+
+number /= 2 # 9
+print(number)
+
+number *= 2 # 18
+print(number)
+
+number -= 2 # 16
+print(number)
+
+number %= 5 # 1
+print(number)
