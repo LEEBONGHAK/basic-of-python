b366c65a2087708d77d0fd5389d256d8a8440af5
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 18:57:09 +0900

    Update heritage(8-5).py

--- chapter8/heritage(8-5).py
+++ chapter8/heritage(8-5).py
@@ -7,9 +7,8 @@ class Unit:
         self.name = name
         self.hp = hp
 
+
 # 공격 유닛
-
-
 class AttackUnit(Unit):
     def __init__(self, name, hp, damage):
         # Unit을 통해 이름과 체력을 받음
@@ -17,8 +16,7 @@ class AttackUnit(Unit):
         self.damage = damage
 
     def attack(self, location):
-        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]"
-              .format(self.name, location, self.damage))
+        print("{0} : {1} 방향으로 적군을 공격합니다. [공격력 {2}]".format(self.name, location, self.damage))
 
     def damaged(self, damage):
         print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
