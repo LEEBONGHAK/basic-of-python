5a67eb286a0da67a4fb0bb6902f1fd7e1c5de231
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-01 19:25:34 +0900

    updated super(8-9).py

--- chapter8/super(8-9).py
+++ chapter8/super(8-9).py
@@ -1,5 +1,5 @@
-# 8-8 pass - 아무 것도 안하고 일단은 넘어간다.
-# 함수 내 사용될 수 있으며, 완성되지 않았지만 완성된 것처럼 생각되며 넘어간다.
+# 8-9 super
+
 
 # 일반 유닛
 class Unit:
@@ -56,20 +56,47 @@ class FlyableAttackUnit(AttackUnit, Flyable):
 # 건물
 class BuildingUnit(Unit):
     def __init__(self, name, hp, location):
-        pass
+        # Unit.__init__(self, name, hp, 0)
+        # super()를 통해 초기화 가능 이때는 self를 쓰지 않음
+        super().__init__(name, hp, 0)
+        self.location = location
 
 
-# 서플라이 디폿 : 건물, 1개 건물 = 8 유닛.
-supply_depot = BuildingUnit("서플라이 디폿", 500, "7시")
+'''
+super()의 문제점
+class Unit:
+    def __init__(self):
+        print("Unit 생성자")
+
+class Flyable:
+    def __init__(self):
+        print("Flyable 생성자")
+
+class FlyableUnit(Unit, Flyable):
+    def __init__(self):
+        super().__init__()
+
+class FlyableUnit1(Flyable, Unit):
+    def __init__(self):
+        super().__init__()
 
 
-def game_start():
-    print("[알림] 새로운 게임을 시작합니다.")
+# 드랍쉽
+dropship = FlyableUnit() # Unit 생성자
+dropship1 = FlyableUnit1() # Flyable 생성자
 
+'''2개 이상의 다중 상속을 쓰는 경우 super() 를 쓰면 순서상 맨 "처음"
+클래스에 대해서 __init__ 함수가 호출 됨'''
 
-def game_over():
-    pass
+class FlyableUnit2(Flyable, Unit):
+    def __init__(self):
+        # super().__init__()
+        Unit.__init__(self)
+        Flyable.__init__(self)
 
+dropship2 = FlyableUnit2()
 
-game_start()
-game_over()
+'''이러한 문제가 있기 때문에 단독 상속 및 모든 부모 클래스에 대해 필요하다면
+super() 를 사용하지 않고 모두 명시하여 초기화 시켜주어야 한다.'''
+
+'''
