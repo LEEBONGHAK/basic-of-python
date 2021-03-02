f0456f49cae6933697007e0f5fc145d8c76f769c
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-02 22:31:58 +0900

    created 퀴즈10(10-9).py and byme.py

---
+++ chapter10/byme.py
@@ -0,0 +1,4 @@
+def sign():
+    print("이 프로그램은 '이봉학'에 의해 만들어졌습니다.")
+    print("유튜브 : http://youtube.com")
+    print("이메일 : dlqhdgkr456@gmail.com")
---
+++ chapter10/퀴즈10(10-9).py
@@ -0,0 +1,17 @@
+'''
+10-9 Quiz #10) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오.
+
+조건 : 모듈 파일명은 byme.py 로 작성
+
+(모듈 사용 예제)
+import byme
+byme.sign()
+
+(출력 예제)
+이 프로그램은 '이봉학'에 의해 만들어졌습니다.
+유튜브 : http://youtube.com
+이메일 : dlqhdgkr456@gmail.com
+'''
+
+import byme
+byme.sign()
