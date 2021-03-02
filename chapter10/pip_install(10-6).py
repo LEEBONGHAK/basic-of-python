ff515f3d9e66c9885d696e4cdef103d850792e9e
LEEBONGHAK <76844576+LEEBONGHAK@users.noreply.github.com>
2021-03-02 22:09:59 +0900

    updated pip_install(10-6).py and created 내장함수(10-7).py

---
+++ chapter10/pip_install(10-6).py
@@ -0,0 +1,19 @@
+# 10-6 pip install
+
+'''
+패키지 설치 방법
+pip install (패키지명)
+ex) pip install pandas
+
+설치되어 있는 패키지 확인
+pip list
+
+패키지의 정보 얻기
+pip show (패키지명)
+
+패키지 업데이트
+pip install --upgrade (패키지명)
+
+패키지 삭제
+pip uninstall (패키지명)
+'''
